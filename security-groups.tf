8c90812849ca90c46b430a543ade9ca1073ee7f6
Rafael Coutinho <rfmarksc@gmail.com>
2021-04-26 12:32:47 -0300

    Add security groups based on Principle of least privilege

--- security-groups.tf
+++ security-groups.tf
@@ -4,22 +4,6 @@ resource "aws_security_group" "eks-cluster-sg" {
   name        = "eks-cluster-sg"
   description = "Principle of least privilege applied to the cluster"
   vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description     = "TLS from node security group"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = aws_security_group.eks-nodes-sg.id
-  }
-
-  egress {
-    from_port       = 10250
-    to_port         = 10250
-    protocol        = "tcp"
-    security_groups = aws_security_group.eks-nodes-sg.id
-  }
-
   tags = {
     Name = "eks-cluster-sg"
   }
@@ -29,33 +13,51 @@ resource "aws_security_group" "eks-nodes-sg" {
   name        = "eks-nodes-sg"
   description = "Principle of least privilege applied to the nodes"
   vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description     = "TCP communication from control plane"
-    from_port       = 10250
-    to_port         = 10250
-    protocol        = "tcp"
-    security_groups = aws_security_group.eks-cluster-sg.id
-  }
-
-  egress {
-    description     = "Allow TLS to control plane"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = aws_security_group.eks-cluster-sg.id
-  }
-
   tags = {
     Name = "eks-nodes-sg"
   }
 }
 
-resource "aws_security_group_rule" "eks-nodes-to-contro-plane" {
-    type = "ingress"
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    security_group_id = "${aws_security_group.bastion.id}"
-    source_security_group_id = "${aws_security_group.private.id}"
+# Security Group rules attached to EKS Cluster security group
+
+resource "aws_security_group_rule" "eks-nodes-tls-to-control-plane" {
+  description              = "TLS from node security group"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks-cluster-sg.id
+  source_security_group_id = aws_security_group.eks-nodes-sg.id
+}
+
+resource "aws_security_group_rule" "eks-control-plane-tcp-to-nodes" {
+  description              = "TCP communication to nodes"
+  type                     = "egress"
+  from_port                = 10250
+  to_port                  = 10250
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks-cluster-sg.id
+  source_security_group_id = aws_security_group.eks-nodes-sg.id
+}
+
+# Security Group rules attached to EKS Node security group
+
+resource "aws_security_group_rule" "eks-nodes-tcp-from-contro-plane" {
+  description              = "TCP communication from Control plane"
+  type                     = "ingress"
+  from_port                = 10250
+  to_port                  = 10250
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks-nodes-sg.id
+  source_security_group_id = aws_security_group.eks-cluster-sg.id
+}
+
+resource "aws_security_group_rule" "eks-nodes-tls-to-contro-plane" {
+  description              = "TCP communication from Control plane"
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks-nodes-sg.id
+  source_security_group_id = aws_security_group.eks-cluster-sg.id
 }
