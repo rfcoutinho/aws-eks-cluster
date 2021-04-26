f6ace341adf11cf35f90a06ae2d108ad6b297d1f
Rafael Coutinho <rfmarksc@gmail.com>
2021-04-26 14:31:53 -0300

    iam roles + policies attachments

--- iam.tf
+++ iam.tf
@@ -1,1 +1,72 @@
 # Define IAM roles and policies to be atteched to AWS EKS resources
+
+## EKS Cluster Role management ##
+resource "aws_iam_role" "eks_cluster_role" {
+  name        = "eks_cluster_role"
+  description = "Allows access to other AWS service resources that are required to operate clusters managed by EKS."
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+  tags = {
+    eks = "cluster"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+
+## EKS Node Role management ##
+resource "aws_iam_role" "eks_node_role" {
+  name        = "eks_node_role"
+  description = "Allows EC2 instances to call AWS services on your behalf."
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+  tags = {
+    eks = "node"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
