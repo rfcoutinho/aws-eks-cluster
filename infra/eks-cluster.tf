dd143ce3e6d417943bbb66980f73930170940b28
Rafael Coutinho <rfmarksc@gmail.com>
2021-04-28 15:07:16 -0300

    Fix missing tag node groups

--- infra/eks-cluster.tf
+++ infra/eks-cluster.tf
@@ -44,8 +44,8 @@ resource "aws_eks_node_group" "node-group-1" {
   instance_types = ["t3a.small", "t3.small"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
