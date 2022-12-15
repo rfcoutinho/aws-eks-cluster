160160de26a8e62f33e108a788a5c2e7ba9ce7e4
Rafael Coutinho <rfmarksc@gmail.com>
2022-12-15 19:40:29 -0300

    update tf modules

--- infra/eks-cluster.tf
+++ infra/eks-cluster.tf
@@ -1,18 +1,24 @@
 module "eks" {
-  source                        = "terraform-aws-modules/eks/aws"
-  cluster_name                  = var.cluster_name
-  cluster_version               = "1.19"
-  subnets                       = module.vpc.private_subnets
-  cluster_security_group_id     = aws_security_group.eks-cluster-sg.id
-  cluster_create_security_group = false
-  cluster_iam_role_name         = aws_iam_role.eks_cluster_role.name
-  manage_cluster_iam_resources  = false
-  manage_worker_iam_resources   = false
+  source                    = "terraform-aws-modules/eks/aws"
+  cluster_name              = var.cluster_name
+  cluster_version           = "1.24"
+  cluster_security_group_id = aws_security_group.eks-cluster-sg.id
 
-  worker_create_security_group = false
-  worker_security_group_id     = aws_security_group.eks-nodes-sg.id
+  # aws-auth configmap
+  create_aws_auth_configmap = true
+  manage_aws_auth_configmap = true
 
-  write_kubeconfig = true
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
   tags = {
     Environment = "challenge"
@@ -20,26 +26,35 @@ module "eks" {
     GithubOrg   = "rfcoutinho"
   }
 
-  vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
-
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      asg_desired_capacity          = 1
-      additional_security_group_ids = [aws_security_group.eks-nodes-sg.id]
-      iam_instance_profile_name     = aws_iam_instance_profile.eks_node_instance_profile.name
-    }
-  ]
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
 
   depends_on = [
     aws_iam_role.eks_cluster_role,
   ]
 
+}
+
+#Commented due the several opened issues to add a custom security group to eks managed nodes
+#https:github.com/aws/containers-roadmap/issues/609
+resource "aws_eks_node_group" "node-group-1" {
+  cluster_name    = var.cluster_name
+  node_group_name = "node-group-1"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = module.vpc.private_subnets
+
+  instance_types = ["t3a.small", "t3.small"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [
+    module.eks.cluster_id,
+  ]
 }
 
 data "aws_eks_cluster" "cluster" {
@@ -49,24 +64,3 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
-
-// Commented due the several opened issues to add a custom security group to eks managed nodes
-// https://github.com/aws/containers-roadmap/issues/609
-// resource "aws_eks_node_group" "node-group-1" {
-//   cluster_name    = var.cluster_name
-//   node_group_name = "node-group-1"
-//   node_role_arn   = aws_iam_role.eks_node_role.arn
-//   subnet_ids      = module.vpc.private_subnets
-
-//   instance_types = ["t3a.small", "t3.small"]
-
-//   scaling_config {
-//     desired_size = 2
-//     max_size     = 3
-//     min_size     = 1
-//   }
-
-//   depends_on = [
-//     module.eks.cluster_id,
-//   ]
-// }
